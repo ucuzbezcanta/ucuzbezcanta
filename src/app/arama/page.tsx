ca377ae8d4898b70097bf023845176709c10d636
ucuzbezcanta <ucuzbezcanta@gmail.com>
2025-07-18 12:00:44 +0300

    Add Logo Intro and Searc Page

--- src/app/arama/page.tsx
+++ src/app/arama/page.tsx
@@ -4,23 +4,19 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-    title: 'Arama sonuçları',
-    description: 'Arama sonuçları sayfası',
+  title: "Arama sonuçları",
+  description: "Arama sonuçları sayfası",
 };
 
-interface SearchPageProps {
-  searchParams: { [key: string]: string | string[] | undefined };
-}
-
-export default async function SearchPage({ searchParams }: SearchPageProps) {
-  const query = searchParams.q;
+export default async function SearchPage(props: any) {
+  const query = props.searchParams?.q;
 
   let products = [];
-  if (typeof query === 'string' && query.trim() !== '') {
+  if (typeof query === "string" && query.trim() !== "") {
     products = await searchProducts(query);
   }
 
-return (
+  return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Arama Sonuçları</h1>
       <p>Arama sorgusu: {query}</p>
@@ -29,17 +25,20 @@ return (
         {products.length > 0 ? (
           products.map((product) => (
             <div key={product.id} className="border p-2 rounded">
-            <Link href={`/urunler/${product.slug}`} key={product.id} className="p-2 rounded block hover:shadow-lg transition-shadow duration-200">
+              <Link
+                href={`/urunler/${product.slug}`}
+                className="p-2 rounded block hover:shadow-lg transition-shadow duration-200"
+              >
                 <Image
-                    src={product.mainImageUrl}
-                    alt={product.name}
-                    width={300}
-                    height={300}
-                    className="w-full h-40 object-cover mb-2 rounded"
-                    />
+                  src={product.mainImageUrl}
+                  alt={product.name}
+                  width={300}
+                  height={300}
+                  className="w-full h-40 object-cover mb-2 rounded"
+                />
                 <h2 className="text-lg font-semibold">{product.name}</h2>
                 <p className="text-gray-600">{product.price} TL</p>
-            </Link>
+              </Link>
             </div>
           ))
         ) : (
